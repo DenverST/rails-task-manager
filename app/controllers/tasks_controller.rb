434efad0edfa19b7eea07ac85b005f04dca11764
Denver Stove <dtstove@gmail.com>
2018-08-07 15:23:40 +1000

    Finished all of the allocated tasks, need to do some style when i get a chance

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,54 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
   end
+
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to index_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    if params["task"]["completed"].to_s == 'true'
+      params["task"]["completed"] = true
+    elsif params["task"]["completed"].to_s == 'false'
+      params["task"]["completed"] = false
+    end
+    @task = Task.find(params[:id])
+    @task.update({completed: params["task"]["completed"]})
+    @task.update(task_params)
+    @task.save!
+    redirect_to show_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to index_path
+  end
+
+  private
+
+  def task_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:task).permit(:title, :details)
+  end
+
 end
